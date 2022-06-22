f95a9920d52a55b9e6e3fc68870af9733a969e14
qianz-z <qianz321@gmail.com>
2022-06-23 00:23:24 +0800

    small updates on positions of component

--- frontend/src/StudyPlan/StudyPlan.js
+++ frontend/src/StudyPlan/StudyPlan.js
@@ -5,10 +5,8 @@ import MindMap from 'react-mindmap'
 const studyplan = () =>{
     return(
         <div className = "studyplan">
-            <h1 className="text-center">
-                Study Plan
-                <MindMap nodes={map.nodes} connections={map.connections} />
-            </h1>
+            <MindMap nodes={map.nodes} connections={map.connections} />
+
         </div>
     )
 }
