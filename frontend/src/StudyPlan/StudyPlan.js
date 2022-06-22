61a995b519a363a07ec3d42373bff232dece4e9c
jeyvia <>
2022-06-22 12:12:31 +0800

    slight updates

--- frontend/src/StudyPlan/StudyPlan.js
+++ frontend/src/StudyPlan/StudyPlan.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { render } from "react-dom";
+import map from "./Map";
+
+const MindMap = require('react-mindmap');
 
 const studyplan = () =>{
     return(
         <div className = "studyplan">
             <h1 className="text-center">
                 Study Plan
+                <MindMap nodes={map.nodes} connections={map.connections} />
             </h1>
         </div>
     )
 }
 
+render(<studyplan />, document.getElementById("root"));
 export default studyplan;
