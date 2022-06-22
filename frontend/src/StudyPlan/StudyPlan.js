50676e77de5a5bf282f33616e2718c49c621760c
jeyvia <>
2022-06-22 14:14:05 +0800

    Study Plan Sample

--- frontend/src/StudyPlan/StudyPlan.js
+++ frontend/src/StudyPlan/StudyPlan.js
@@ -1,9 +1,7 @@
 import React from 'react'
-<<<<<<< HEAD
 import { render } from "react-dom";
 import map from "./Map";
-
-const MindMap = require('react-mindmap');
+import MindMap from 'react-mindmap'
 
 const studyplan = () =>{
     return(
@@ -12,15 +10,6 @@ const studyplan = () =>{
                 Study Plan
                 <MindMap nodes={map.nodes} connections={map.connections} />
             </h1>
-=======
-import './StudyPlan.css';
-
-const studyplan = () =>{
-    return(
-        <div className='studyplan'>
-            <h1>Study Plan</h1>
-            <p>hello</p>
->>>>>>> bd3797f7ad108678baa1bf2d576ae0b74025073d
         </div>
     )
 }
