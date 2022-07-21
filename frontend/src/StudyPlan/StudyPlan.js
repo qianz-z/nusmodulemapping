44a1d63ec6fab8fa1bb85beb1a298c37ec9144fc
jeyvia <>
2022-07-21 15:04:35 +0800

    study plan fix

--- frontend/src/StudyPlan/StudyPlan.js
+++ frontend/src/StudyPlan/StudyPlan.js
@@ -12,25 +12,25 @@ function Studyplan(props) {
     Mods.map((item) => {
         let y1, y2, y3, y4, y5 = 100;
         if (item.prereq.length === 0) {
-            node.push({ text: item.code, fx: 100, fy: y1, url: "/modules/" + item.code, category: "" })
+            node.push({ text: item.code, fx: 100, fy: y1, url: "#/modules/" + item.code, category: "" })
             y1 += 100;
         }
         else {
             let temp = item.prereq[item.prereq.length - 1];
             if (temp[2] === '1') {
-                node.push({ text: item.code, fx: 300, fy: y2, url: "/modules/" + item.code, category: "" });
+                node.push({ text: item.code, fx: 300, fy: y2, url: "#/modules/" + item.code, category: "" });
                 y2 += 100;
             }
             else if (temp[2] === '2') {
-                node.push({ text: item.code, fx: 500, fy: y3, url: "/modules/" + item.code, category: "" });
+                node.push({ text: item.code, fx: 500, fy: y3, url: "#/modules/" + item.code, category: "" });
                 y3 += 100;
             }
             else if (temp[2] === '3') {
-                node.push({ text: item.code, fx: 700, fy: y4, url: "/modules/" + item.code, category: ""});
+                node.push({ text: item.code, fx: 700, fy: y4, url: "#/modules/" + item.code, category: ""});
                 y4 += 100;
             }
             else {
-                node.push({ text: item.code, fx: 900, fy: y5, url: "/modules/" + item.code, category: "" });
+                node.push({ text: item.code, fx: 900, fy: y5, url: "#/modules/" + item.code, category: "" });
                 y5 += 100;
             }
         };
